8763ffbd602c233514bda4bbb7f2558d87452bac
Nathan Wentworth <nwentworth95@gmail.com>
2017-08-01 14:07:51 -0400

    refactored a ton of code, cleaned up structure

--- renderer.js
+++ renderer.js
@@ -16,35 +16,37 @@ let currentPath = rootDirectory;
 let imageElements = [];
 
 // container elements
-let mainContainer;
-let leftSide;
-let rightSide;
-let folderView;
-let imageView;
+let mainContainer = document.getElementById('main');
+let leftSide = document.getElementById('left');
+let rightSide = document.getElementById('right');
+let folderView = document.getElementById('folders');
+let imageView = document.getElementById('images');
 
 // option/input elements
-let optionsButton;
-let optionsMenu;
-let directoryInputSubmit;
+let optionsMenu = document.getElementById('options-menu');
+let openFolderCtrl = document.getElementById('open-folder-ctrl');
 
 let columnOption = {
-  // ctrl, label
+  ctrl: document.getElementById('image-width-ctrl'),
+  label: document.getElementById('image-width-label')
 }
 
 let gutterOption = {
-  // ctrl, label
+  ctrl: document.getElementById('gutter-width-ctrl'),
+  label: document.getElementById('gutter-width-label')
 }
 
-let backgroundOption = {
-  // ctrl, label
-}
-
-let userStylesOption;
-let hideOptionsButton;
-let helpButton;
-let infoButton;
-let howToDialog
-let infoDialog
+let backgroundOptionCtrl = document.getElementById('background-ctrl');
+let userStylesCtrl = document.getElementById('user-styles-ctrl');;
+let userStylesElem = document.getElementById('user-styles');
+let hideSidePanelCtrl = document.getElementById('hide-side-panel-ctrl');
+let hideOptionsCtrl = document.getElementById('options-ctrl');
+let helpButton = document.getElementById('help-ctrl');
+let infoButton = document.getElementById('info-ctrl');
+let howToDialog = document.getElementById('how-to');
+let infoDialog = document.getElementById('info');
+let howToCloseCtrl = document.getElementById('how-to-close-ctrl');
+let infoCloseCtrl = document.getElementById('info-close-ctrl');
 
 // global options
 let options = {
@@ -56,40 +58,60 @@ let options = {
 let lastDirectory = currentPath;
 
 // other elements
-let title;
+let title = document.getElementsByTagName('title')[0];
 
 // this stores the directory structure for both
 // caching access, and client-side access
 let leaves = [];
 
-function Init() {
-  Load();
+// on load, run the init and the loadfiles functions
+window.addEventListener('load', function () {
+  InitialLoad();
+});
 
-  mainContainer = document.getElementById('main');
-  leftSide = document.getElementById('left');
-  rightSide = document.getElementById('right');
-  folderView = document.getElementById('folders');
-  imageView = document.getElementById('images');
-  title = document.getElementsByTagName('title')[0];
-  userStylesElem = document.getElementById('user-styles');
+function InitialLoad() {
+  console.log('~~~~~~~~~ welcome to moodbored ~~~~~~~~~');
+  LoadOptions();
+  AddEventsToMainButtons();
+  AddEventsToOptionsButtons();
+  SetAllLinksExternal();
+  SetVersionInfo();
 
-  directoryInputSubmit = document.getElementById('directoryInputSubmit');
+  let loadedPath = localStorage.getItem('lastDirectory');
+  rootDirectory = localStorage.getItem('rootDirectory');
+  console.log('> root is ' + rootDirectory);
 
-  directoryInputSubmit.addEventListener('click', function() {
-    OpenFolder();
+  if (rootDirectory == null || rootDirectory == '') {
+    HowToDialogToggle();
+  } else {
+    GetNewDirectoryStructure(rootDirectory);
+    CreateFolderView();
+
+    if (loadedPath != null) {
+      LoadDirectoryContents(loadedPath);
+    } else if (rootDirectory != '') {
+      LoadDirectoryContents(rootDirectory);
+    }
+  }
+}
+
+
+function AddEventsToMainButtons() {
+  openFolderCtrl.addEventListener('click', function() {
+    OpenNewRootFolder();
   });
 
-  hideOptionsButton = document.getElementById('hide-options-button');
-  hideOptionsButton.addEventListener('click', function () {
+  hideOptionsCtrl.addEventListener('click', function () {
+    ToggleSection(optionsMenu);
+  });
+
+  hideSidePanelCtrl.addEventListener('click', function () {
     ToggleSection(leftSide);
     ToggleImageContainerSize();
   });
-
 }
 
-function InitOptionControllers() {
-  columnOption.ctrl = document.getElementById('image-width-ctrl');
-  columnOption.label = document.getElementById('image-width-label');
+function AddEventsToOptionsButtons() {
   columnOption.label.innerText = options.columns;
   columnOption.ctrl.value = options.columns;
   columnOption.ctrl.addEventListener('input', function() {
@@ -98,11 +120,9 @@ function InitOptionControllers() {
     if (imageElements.length > 0) {
       ResizeImages();
     }
-    Save();
+    SaveOptions();
   });
 
-  gutterOption.ctrl = document.getElementById('gutter-width-ctrl');
-  gutterOption.label = document.getElementById('gutter-width-label');
   gutterOption.label.innerText = options.gutter;
   gutterOption.ctrl.value = options.gutter;
   gutterOption.ctrl.addEventListener('input', function() {
@@ -111,56 +131,47 @@ function InitOptionControllers() {
     if (imageElements.length > 0) {
       ResizeImages();
     }
-    Save();
+    SaveOptions();
   });
 
-  backgroundOption.ctrl = document.getElementById('background-ctrl');
-  backgroundOption.label = document.getElementById('background-label');
-  backgroundOption.label.innerText = options.background;
-  backgroundOption.ctrl.value = options.background;
+  backgroundOptionCtrl.value = options.background;
   mainContainer.style.backgroundColor = options.background;
-  backgroundOption.ctrl.addEventListener('input', function() {
-    options.background = backgroundOption.ctrl.value;
-    backgroundOption.label.innerText = backgroundOption.ctrl.value;
+  backgroundOptionCtrl.addEventListener('input', function() {
+    options.background = backgroundOptionCtrl.value;
     mainContainer.style.backgroundColor = options.background;
-    Save();
+    SaveOptions();
   });
 
-  userStylesOption = document.getElementById('user-styles-ctrl');
-  userStylesOption.value = options.userStyles;
+  userStylesCtrl.value = options.userStyles;
   userStylesElem.innerText = options.userStyles;
-  userStylesOption.addEventListener('input', function() {
-    options.userStyles = userStylesOption.value;
-    console.log(userStylesOption.value);
+  userStylesCtrl.addEventListener('input', function() {
+    options.userStyles = userStylesCtrl.value;
     userStylesElem.innerText = options.userStyles;
-    Save();
+    SaveOptions();
   });
 
-  helpButton = document.getElementById('help-ctrl');
   helpButton.addEventListener('click', function () {
-    HowToDialogToggle();
+    ToggleSection(howToDialog);
   });
 
-  howToDialog = document.getElementById('how-to');
-  document.getElementById('how-to-close-ctrl').addEventListener('click', function () {
-    howToDialog.classList.toggle('hidden');
+  howToCloseCtrl.addEventListener('click', function () {
+    ToggleSection(howToDialog);
   });
 
-  infoButton = document.getElementById('info-ctrl');
   infoButton.addEventListener('click', function () {
-    InfoDialogToggle();
+    ToggleSection(infoDialog);
   });
 
-  infoDialog = document.getElementById('info');
-  document.getElementById('info-close-ctrl').addEventListener('click', function () {
-    infoDialog.classList.toggle('hidden');
-    console.log('toggled infoDialog');
+  infoCloseCtrl.addEventListener('click', function () {
+    ToggleSection(infoDialog);
   });
-
-
 }
 
-function OpenFolder() {
+function SetVersionInfo() {
+  document.getElementById('version-disp').innerText = require('electron').remote.app.getVersion();
+}
+
+function OpenNewRootFolder() {
   dialog.showOpenDialog({properties: ["openDirectory"]}, (folder) => {
     if (folder === undefined) {
       console.log("no file selected");
@@ -168,16 +179,14 @@ function OpenFolder() {
     } else {
       console.log(folder);
       let _root = folder[0];
-      leaves = [];
-      ClearChildren(folderView);
-      GetDirectories(_root);
-      LoadDirectory(_root, true);
+      GetNewDirectoryStructure(_root);
+      LoadDirectoryContents(_root, true);
       CreateFolderView();
     }
   })
 }
 
-function Load() {
+function LoadOptions() {
   _options = localStorage.getItem('options');
   if (_options != null) {
     options = JSON.parse(_options);
@@ -187,47 +196,9 @@ function Load() {
   }
 }
 
-function Save() {
+function SaveOptions() {
   localStorage.setItem('options', JSON.stringify(options));
   console.log('saved options');
-}
-
-// on load, run the init and the loadfiles functions
-window.addEventListener('load', function () {
-  InitialLoad();
-});
-
-function InitialLoad() {
-  console.log('~~~~~~~~~ initial load ~~~~~~~~~');
-  Init();
-  InitOptionControllers();
-  SetAllLinksExternal();
-  document.getElementById('version-disp').innerText = require('electron').remote.app.getVersion();
-  let loadedPath = localStorage.getItem('lastDirectory');
-  rootDirectory = localStorage.getItem('rootDirectory');
-  console.log('root is ' + rootDirectory);
-  if (rootDirectory == null) {
-    HowToDialogToggle();
-  } else {
-    GetDirectories(rootDirectory);
-    CreateFolderView();
-
-    if (loadedPath != null) {
-      LoadDirectory(loadedPath);
-    } else if (rootDirectory != '') {
-      LoadDirectory(rootDirectory);
-    } else {
-      console.log('no directory to load, please open one with "open folder"');
-    }
-  }
-}
-
-function HowToDialogToggle() {
-  howToDialog.classList.toggle('hidden');
-}
-
-function InfoDialogToggle() {
-  infoDialog.classList.toggle('hidden');
 }
 
 function SetAllLinksExternal() {
@@ -242,74 +213,74 @@ function SetAllLinksExternal() {
   }
 }
 
-function GetDirectories(dirPath) {
-  let dir = fs.readdirSync(dirPath);
+// recursive function that gets the new directories.
+// pushes all tail/endpoint directories into `leaves`
+function GetNewDirectoryStructure(path) {
+  let dir = fs.readdirSync(path);
   if (dir != undefined && dir.length > 0) {
     for (let i = 0; i < dir.length; i++) {
       let file = dir[i];
       let notLeaf = false;
 
-      let stat = fs.lstatSync(dirPath + '//' + file);
+      let stat = fs.lstatSync(path + '//' + file);
       if (!stat.isDirectory()) {
         if (i === dir.length - 1 && !notLeaf) {
-          leaves.push(dirPath);
+          leaves.push(path);
           // console.log(leaves[leaves.length - 1]);
           // because it reached the end of the loop and there's no directories,
           // it must be full of images
-          console.log(dirPath + " is a leaf");
-          // CreateFolderElement(dirPath);
+          console.log(path + " is a leaf");
+          // CreateFolderElement(path);
         }
       } else {
-        let totalPath = dirPath + '/' + file;
+        let totalPath = path + '/' + file;
         // because there's a directory, it cannot be an end folder
         notLeaf = true;
         // starts the function again in the child directory it found
-        GetDirectories(totalPath);
+        GetNewDirectoryStructure(totalPath);
       }
     }
   }
-
 }
 
 function CreateFolderView() {
   ClearChildren(folderView);
-  leaves.sort();
   if (leaves.length > 1) {
+    leaves.sort();
     for (let leaf of leaves) {
-      // console.log(leaf);
       CreateFolderElement(leaf);
     }
   }
 }
 
 function CreateFolderElement(totalPath) {
-  let sp = document.createElement('span');
+  let sp = document.createElement('button');
   let spTextContent = totalPath.replace(rootDirectory + "/", "");
   spTextContent = spTextContent.replace("/", " / ");
   let spText = document.createTextNode(spTextContent);
   sp.appendChild(spText);
 
   sp.addEventListener('click', function() {
-    LoadDirectory(totalPath);
+    LoadDirectoryContents(totalPath);
   });
 
   folderView.appendChild(sp);
 }
 
-function LoadDirectory(dirPath, newRoot) {
+function LoadDirectoryContents(path, newRoot) {
   if (newRoot) {
-    rootDirectory = dirPath;
+    rootDirectory = path;
     localStorage.setItem('rootDirectory', rootDirectory);
     console.log('new root: ' + rootDirectory);
     leaves = [];
-    GetDirectories(dirPath);
+    GetNewDirectoryStructure(path);
   }
 
-  console.log('loading ' + dirPath);
-  title.innerText = moodbored + " - " + dirPath;
+  console.log('loading ' + path);
+  title.innerText = moodbored + " - " + path;
 
-  if (dirPath != currentPath) {
-    currentPath = dirPath;
+  if (path != currentPath) {
+    currentPath = path;
     ClearChildren(imageView);
     imageElements = [];
 
@@ -326,22 +297,16 @@ function LoadDirectory(dirPath, newRoot) {
             if (err) {
               return console.error(file + ': ' + err);
             }
-
-            if (!stats.isDirectory() &&
-                file.match(/.(jpg|png|gif|jpeg|bmp|webp|svg)/)
-                )
-            {
+            let imgFileTypes = /.(jpg|png|gif|jpeg|bmp|webp|svg)/;
+            if (!stats.isDirectory() && file.match(imgFileTypes)) {
               CreateImage(_path, file);
             }
           })
         }
-      } else {
-        console.log(dir + ' length is 0!');
       }
     })
   }
 }
-
 
 function CreateImage(path, file) {
   let img = document.createElement('img');
@@ -357,6 +322,8 @@ function CreateImage(path, file) {
   }
   imageElements.push(img);
 }
+
+// ~~~~~~~~~ utility functions ~~~~~~~~~
 
 function ResizeImages() {
   for (let img of imageElements) {
